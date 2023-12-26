4bf1f263d355af89d96fb41f956fa00046297823
siyal343 <siyal343@gmail.com>
2023-12-26 18:23:59 +0500

    added more functionality

--- main.py
+++ main.py
@@ -10,13 +10,12 @@ import asyncio
 
 
 async def load_url(session: AsyncHTMLSession, url: str) -> BeautifulSoup:
-    response = await session.get(url)
-
-
-    await response.html.arender(sleep=3, scrolldown=15, keep_page=True)
-
-
-    return BeautifulSoup(response.html.html, "html.parser")
+    try:
+        response = await session.get(url)
+        await response.html.arender(sleep=2, scrolldown=15, keep_page=True)
+        return BeautifulSoup(response.html.html, "html.parser")
+    except Exception as e:
+        print(e)
 
 
 def gather_data(soup: BeautifulSoup) -> list:
@@ -40,30 +39,13 @@ def gather_data(soup: BeautifulSoup) -> list:
 
     for row in table_rows[2:]:
         coin_logo = row.find("img", {"class": "coin-logo"})["src"]
-
-        name_tag = row.find("p", {"class": "sc-4984dd93-0 kKpPOn"})
-        coin_name = name_tag.text
-
-        ticker_tag = row.find("p", {"class": "coin-item-symbol"})
-        coin_ticker = ticker_tag.text
-
-        price_tag = row.find("div", {"class": "sc-a0353bbc-0"})
-        coin_price = price_tag.text
-
-        change_tag = row.find_all("span", {"class": "sc-d55c02b-0"})
-        change_1hr = change_tag[0].text
-        change_24hr = change_tag[1].text
-        change_7d = change_tag[2].text
-
-        market_cap_tag = row.find("span", {"class": "sc-7bc56c81-0"})
-        market_cap = market_cap_tag.text
-
-        volume_24hr_tag = row.find(
-            "p", {"class": "sc-4984dd93-0 jZrMxO font_weight_500"}
-        )
-        volume_24hr = volume_24hr_tag.text
-        circ_supply_tag = row.find("p", {"class": "sc-4984dd93-0 WfVLk"})
-        circ_supply = circ_supply_tag.text
+        coin_name = row.find("p", {"class": "sc-4984dd93-0 kKpPOn"}).text
+        coin_ticker = row.find("p", {"class": "coin-item-symbol"}).text
+        coin_price = row.find("div", {"class": "sc-a0353bbc-0"}).text
+        change_1hr, change_24hr, change_7d = [tag.text for tag in row.find_all("span", {"class": "sc-d55c02b-0"})]
+        market_cap = row.find("span", {"class": "sc-7bc56c81-0"}).text
+        volume_24hr = row.find("p", {"class": "sc-4984dd93-0 jZrMxO font_weight_500"}).text
+        circ_supply = row.find("p", {"class": "sc-4984dd93-0 WfVLk"}).text
 
         tempCoin = Coin(
             coin_logo=coin_logo,
@@ -83,19 +65,20 @@ def gather_data(soup: BeautifulSoup) -> list:
     return coins
 
 async def main():
-
     base_url = "https://coinmarketcap.com/"
-
     pages_to_scrape = 10
-
-    url = base_url + '?page=' + str(pages_to_scrape)
-
     session = AsyncHTMLSession()
 
+    bsHtml = []
+
     for i in range(1, pages_to_scrape + 1):
-        url = base_url + '?page=' + str(i)
+        url = f"{base_url}?page={i}"
         soup = await load_url(session, url)
-        print(gather_data(soup))
+        bsHtml.extend(soup)
+        
+        
+        
+    print(gather_data(soup))
 
     await session.close()
 
