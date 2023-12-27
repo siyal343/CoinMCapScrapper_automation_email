0f6e3f233a3dd7024df0244cdb735a7fdbb81a41
siyal343 <siyal343@gmail.com>
2023-12-27 19:02:34 +0500

    Working Script (requirements.txt not final/issues)

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-
-from requests_html import AsyncHTMLSession
+from requests_html import HTMLSession, AsyncHTMLSession
 from bs4 import BeautifulSoup
 from pydantic import BaseModel
 import asyncio
+from typing import Optional
+import pandas as pd
+import datetime
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
-
-
-
+EMAIL_ADDRESS = os.environ.get("EMAIL_USER")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASS")
 
 
 async def load_url(session: AsyncHTMLSession, url: str) -> BeautifulSoup:
@@ -17,20 +24,19 @@ async def load_url(session: AsyncHTMLSession, url: str) -> BeautifulSoup:
     except Exception as e:
         print(e)
 
-
 def gather_data(soup: BeautifulSoup) -> list:
-
     class Coin(BaseModel):
-        coin_logo: str
-        coin_name: str
-        coin_ticker: str
-        coin_price: str
-        change_1hr: str
-        change_24hr: str
-        change_7d: str
-        market_cap: str
-        volume_24hr: str
-        circ_supply: str
+        coin_rank: Optional[int]
+        coin_logo: Optional[str]
+        coin_name: Optional[str]
+        coin_ticker: Optional[str]
+        coin_price: Optional[str]
+        change_1hr: Optional[str]
+        change_24hr: Optional[str]
+        change_7d: Optional[str]
+        market_cap: Optional[str]
+        volume_24hr: Optional[str]
+        circ_supply: Optional[str]
 
     coins = []
 
@@ -38,16 +44,53 @@ def gather_data(soup: BeautifulSoup) -> list:
     table_rows = table.find_all("tr")
 
     for row in table_rows[2:]:
-        coin_logo = row.find("img", {"class": "coin-logo"})["src"]
-        coin_name = row.find("p", {"class": "sc-4984dd93-0 kKpPOn"}).text
-        coin_ticker = row.find("p", {"class": "coin-item-symbol"}).text
-        coin_price = row.find("div", {"class": "sc-a0353bbc-0"}).text
-        change_1hr, change_24hr, change_7d = [tag.text for tag in row.find_all("span", {"class": "sc-d55c02b-0"})]
-        market_cap = row.find("span", {"class": "sc-7bc56c81-0"}).text
-        volume_24hr = row.find("p", {"class": "sc-4984dd93-0 jZrMxO font_weight_500"}).text
-        circ_supply = row.find("p", {"class": "sc-4984dd93-0 WfVLk"}).text
+        coin_rank = (
+            row.find("p", {"class": "sc-4984dd93-0 iWSjWE"}).text
+            if row.find("p", {"class": "sc-4984dd93-0 iWSjWE"})
+            else None
+        )
+        coin_logo = (
+            row.find("img", {"class": "coin-logo"})["src"]
+            if row.find("img", {"class": "coin-logo"})
+            else None
+        )
+        coin_name = (
+            row.find("p", {"class": "sc-4984dd93-0 kKpPOn"}).text
+            if row.find("p", {"class": "sc-4984dd93-0 kKpPOn"})
+            else None
+        )
+        coin_ticker = (
+            row.find("p", {"class": "coin-item-symbol"}).text
+            if row.find("p", {"class": "coin-item-symbol"})
+            else None
+        )
+        coin_price = (
+            row.find("div", {"class": "sc-a0353bbc-0"}).text
+            if row.find("div", {"class": "sc-a0353bbc-0"})
+            else None
+        )
+        change_1hr, change_24hr, change_7d = [
+            tag.text if tag else None
+            for tag in row.find_all("span", {"class": "sc-d55c02b-0"})
+        ]
+        market_cap = (
+            row.find("span", {"class": "sc-7bc56c81-0"}).text
+            if row.find("span", {"class": "sc-7bc56c81-0"})
+            else None
+        )
+        volume_24hr = (
+            row.find("p", {"class": "sc-4984dd93-0 jZrMxO font_weight_500"}).text
+            if row.find("p", {"class": "sc-4984dd93-0 jZrMxO font_weight_500"})
+            else None
+        )
+        circ_supply = (
+            row.find("p", {"class": "sc-4984dd93-0 WfVLk"}).text
+            if row.find("p", {"class": "sc-4984dd93-0 WfVLk"})
+            else None
+        )
 
         tempCoin = Coin(
+            coin_rank=int(coin_rank),
             coin_logo=coin_logo,
             coin_name=coin_name,
             coin_ticker=coin_ticker,
@@ -64,27 +107,76 @@ def gather_data(soup: BeautifulSoup) -> list:
 
     return coins
 
-async def main():
+async def extract_data(session: AsyncHTMLSession):
     base_url = "https://coinmarketcap.com/"
     pages_to_scrape = 10
-    session = AsyncHTMLSession()
+    session = session
 
-    bsHtml = []
+    coins = []
 
     for i in range(1, pages_to_scrape + 1):
         url = f"{base_url}?page={i}"
         soup = await load_url(session, url)
-        bsHtml.extend(soup)
-        
-        
-        
-    print(gather_data(soup))
+        coins = coins + gather_data(soup)
 
     await session.close()
 
+    return coins
+
+def get_dateTime():
+    now = datetime.datetime.now()
+    dt_string = now.strftime("%d-%m-%Y_%H%M%S")
+    return dt_string
+
+def save_to_excel(Coins, currentDateTime):
+    data = [coin.dict() for coin in Coins]
+    df = pd.DataFrame(data)
+    # get the current date and time to append the excel file name
+    dt_string = currentDateTime
+    df.to_excel(f"Coins_{dt_string}.xlsx", index=False)
+
+
+def email_excel(currentDateTime):
+    msg = MIMEMultipart()
+
+    msg["Subject"] = "Top 1000 Cryptocurrencies"
+
+    msg["From"] = EMAIL_ADDRESS
+
+    msg["To"] = "siyal343@gmail.com"
+
+    body = "Scrapped 1000 Cryptocurrencies Rankings"
+
+    msg.attach(MIMEText(body, "plain"))
+
+    with open(f"Coins_{currentDateTime}.xlsx", "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header(
+            "Content-Disposition", f"attachment; filename= Coins_{currentDateTime}.xlsx"
+        )
+        msg.attach(part)
+
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port=465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+        smtp.send_message(msg)
+
+    os.remove(f"Coins_{currentDateTime}.xlsx")
+
+
+async def main():
+    session = AsyncHTMLSession()
+    return await extract_data(session)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    Coins = asyncio.run(main())
+    print(f"there were {len(Coins)} coins extracted")
+    currentDateTime = get_dateTime()
+    save_to_excel(Coins, currentDateTime)
+    email_excel(currentDateTime)    
 
 
